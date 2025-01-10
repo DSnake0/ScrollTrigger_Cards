651604218a27dcea40ad0871e2f2a076d875eb0e
DSnake0 <revilwhoami0098@gmail.com>
2025-01-10 15:29:02 +0500

    Add scrollTrigger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-// Initialize Lenis
 const lenis = new Lenis({
     autoRaf: true,
 });
 
-// Listen for the scroll event and log the event data
 lenis.on('scroll', (e) => {
     console.log(e);
 });
+
+const newCard = document.querySelectorAll(".card").forEach(newCard => {
+    gsap.to(newCard, {
+        opacity: 0,
+        scale: 0.7,
+        scrollTrigger: {
+            trigger: newCard,
+            start: "top 15%",
+            end: "bottom 15%",
+            scrub: 1,
+            markers: true
+        }
+    })
+})
